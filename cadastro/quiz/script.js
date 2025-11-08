ee458fb122ccd7d3e560c41850fab960d67c7179
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-11-08 16:02:30 -0300

    Add files via upload

--- cadastro/quiz/script.js
+++ cadastro/quiz/script.js
@@ -8,6 +8,16 @@ let questions = [
     ["Oque é um horizonte de eventos?", "Horizonte de eventos é o ponto onde o tempo para completamente ao redor de um buraco negro.", "É a camada de gás e poeira que envolve o buraco negro, responsável por esconder a luz que tenta sair.", "É a distância máxima em que um buraco negro pode sugar objetos ao seu redor.", "É o ponto em que nem a luz escapa da sua gravidade"]
 ]
 
+
+let lista = [1, 34, 56, 4, 8, 93, 23, 66, 79]
+
+lista.sort(function() {
+    return Math.random() - 0.5
+})
+
+console.log(lista)
+
+
 function verificar() {
 
     let select = document.querySelector("input[name='resposta1']:checked")
@@ -21,7 +31,6 @@ function verificar() {
 
     counter++
   
-
     if (counter == 1) {
         h1.innerHTML = questions[0][0]
         allLabel[0].innerHTML = `<input name="resposta1" type="radio" id="rsp1" value="false"> ${questions[0][1]}`
@@ -65,6 +74,4 @@ function verificar() {
 function finalizar() {
     // link do meu outro html, ? procura, pt vira uma variavel com a quantidade de pontos da variavel points
     window.location.href = `pontos.html?pt=${points}`
-
-    
 }
